d22697c2fde3a6f8c0dcf2fbe10c10cb05b60607
Jeff <jm@jeffz.com>
2020-06-23 11:41:48 -0400

    Added Quiz Controller

---
+++ Quizzer/Controllers/QuizController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Quizzer.Controllers
+{
+    public class QuizController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
