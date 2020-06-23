809c4499d6ff25c9dad07c90352e5728187f55e3
Jeff <jm@jeffz.com>
2020-06-23 11:22:39 -0400

    Add Question Model

---
+++ Quizzer/Models/Question.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Quizzer.Models
+{
+    public class Question
+    {
+        public string QuestionText { get; set; }
+
+        public List<string> Choices { get; set; } = new List<string>();
+
+        public string StudentAnswer { get; set; }
+
+        public string CorrectAnswer { get; set; }
+    }
+}
