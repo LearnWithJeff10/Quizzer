cf23f8dd70366459753a75aa40e7d9b88bc9fc99
Jeff <jm@jeffz.com>
2020-06-23 11:57:37 -0400

    Add QuizController and provide access to the QuizzerRepository. Add a default Index View.

--- Quizzer/Controllers/QuizController.cs
+++ Quizzer/Controllers/QuizController.cs
@@ -3,14 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Quizzer.Services;
 
 namespace Quizzer.Controllers
 {
     public class QuizController : Controller
     {
+        private readonly IQuizzerRepository _quizzerRepository;
+
+        public QuizController(IQuizzerRepository quizzerRepository)
+        {
+            _quizzerRepository = quizzerRepository;
+        }
+
         public IActionResult Index()
         {
-            return View();
+            return View(_quizzerRepository.Questions);
         }
     }
 }
