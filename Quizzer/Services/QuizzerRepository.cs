22db0f0fda171d2539c13476d2462b9833081f2a
Jeff <jm@jeffz.com>
2020-06-23 11:28:35 -0400

    Add Quiz Repository

---
+++ Quizzer/Services/QuizzerRepository.cs
@@ -0,0 +1,14 @@
+﻿using Quizzer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quizzer.Services
+{
+    public class QuizzerRepository
+    {
+        public List<Question> Questions{ get; set; }
+    }
+}
